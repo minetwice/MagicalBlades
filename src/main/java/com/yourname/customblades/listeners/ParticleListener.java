2b91aa3b17f649fccbc7e3e25dd13d5e003f10c6
Twicefear <ytd82774@gmail.com>
2026-01-13 11:18:33 +0530

    Update ParticleListener.java

--- src/main/java/com/yourname/customblades/listeners/ParticleListener.java
+++ src/main/java/com/yourname/customblades/listeners/ParticleListener.java
@@ -1,1 +1,16 @@
+package com.yourname.customblades.listeners;
 
+import com.yourname.customblades.CustomBlades;
+import org.bukkit.event.Listener;
+
+public class ParticleListener implements Listener {
+    
+    private final CustomBlades plugin;
+    
+    public ParticleListener(CustomBlades plugin) {
+        this.plugin = plugin;
+    }
+    
+    // Particle effects are handled by ParticleManager
+    // This listener can be extended for future particle-related events
+}
